409802c59008a3f31e78b5ab9a0907150d28e8af
Tony Lea <tonylea@deadrobot.co.uk>
2018-10-14 09:32:54 +0100

    Created single and photogrid components

---
+++ client/components/Single.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Single extends Component {
+  render() {
+    return <div className="single-photo">I'm the Single</div>;
+  }
+}
+
+export default Single;
